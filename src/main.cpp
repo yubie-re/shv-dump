b80f9bc573740842ceec0908f681fc8fcc01d3eb
yubie <24496817+yubie-re@users.noreply.github.com>
2023-02-06 19:33:36 -0500

    refactor dumping method

--- src/main.cpp
+++ src/main.cpp
@@ -3,44 +3,43 @@
 #include "MinHook.h"
 
 bool g_dumped = false;
+int64_t* g_native_table = 0;
+int g_update_count = 0;
+int g_max_natives = 0;
+
+/*
+The table structure looks something like this
+int64_t native_table[NATIVES_COUNT][UPDATE_COUNT]
+Not really any clean way as we need to scan for the sizes at runtime but we can work with it.
+*/
+
 DWORD (*o_get_module_name)(HMODULE mod, LPSTR filename, DWORD size);
 DWORD get_module_name_hk(HMODULE mod, LPSTR filename, DWORD size)
 {
     if (mod == 0 || g_dumped)
         return o_get_module_name(mod, filename, size);
-    auto first_map = (int64_t *)scanner::rip(scanner::scan("4C 8D 15 ? ? ? ? 33 D2 49 8B CA 44 8B CA", "First Map", mod), 3);
-    auto update_count = *(int*)((uintptr_t)(scanner::scan("48 81 C1 ? ? ? ? 49 63 C1 48 3D", "Update Count", mod)) + 3) / sizeof(int64_t);
-    auto total_natives = *(int*)((uintptr_t)(scanner::scan("48 3D ? ? ? ? 72 E6", "Total Natives", mod)) + 2);
-
-    std::unordered_map<int64_t, int64_t> crossmap;
-    std::unordered_map<int64_t, int64_t> crossupdate; // so we can backtrack update to update (it is stored basically as crosstables for each update)
-
-    for (int i = 0; i < update_count - 1; i++)
+    g_native_table = (int64_t *)scanner::rip(scanner::scan("4C 8D 15 ? ? ? ? 33 D2 49 8B CA 44 8B CA", "First Map", mod), 3);
+    g_update_count = *(int*)((uintptr_t)(scanner::scan("48 81 C1 ? ? ? ? 49 63 C1 48 3D", "Update Count", mod)) + 3) / sizeof(int64_t);
+    g_max_natives = *(int*)((uintptr_t)(scanner::scan("48 3D ? ? ? ? 72 E6", "Total Natives", mod)) + 2);
+    std::vector<std::pair<int64_t, int64_t>> crossmap;
+    for(size_t native_index = 0; native_index < g_max_natives; native_index++)
     {
-        auto map = first_map + i;
-        for(size_t native_index = 0; native_index < total_natives; native_index++, map += (update_count))
+        int64_t* native_update_array = &g_native_table[native_index * g_update_count];
+        int64_t initial_hash = 0;
+        for(int i = 0; i < g_update_count; i++)
         {
-            auto new_hash = *(int64_t *)(first_map + update_count * native_index + (i + 1));
-            auto old_hash = *map;
-            if(new_hash == 0)
-                continue;
-            if (crossupdate.contains(old_hash) && !crossupdate.contains(new_hash))
+            if(native_update_array[i] != 0)
             {
-                crossmap[crossupdate[old_hash]] = new_hash;
-                crossupdate[new_hash] = crossupdate[old_hash];
-            }
-            else
-            {
-                if (old_hash == 0)
-                    old_hash = new_hash;
-                crossmap[old_hash] = new_hash;
-                crossupdate[new_hash] = old_hash;
+                initial_hash = native_update_array[i];
+                break;
             }
         }
+        if(native_update_array[g_update_count - 1] != 0)
+        {
+            crossmap.push_back(std::make_pair(initial_hash, native_update_array[g_update_count - 1]));
+        }
     }
-
     g_dumped = true;
-    
     for(auto& [o, n] : crossmap)
     {
         printf("{ 0x%016llX, 0x%016llX },\n", o, n);
